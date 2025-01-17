a9f5a8fbfe2445ec12a058fbaa7de7599d6defb0
Clancy-dev <clancyro1789@gmail.com>
2025-01-17 12:37:48 +0300

    the

--- components/Products.tsx
+++ components/Products.tsx
@@ -71,7 +71,6 @@ function ProductCard({ product }: ProductCardProps) {
 
   return (
     <div
-      ref={targetRef}
       className={`bg-white rounded-lg shadow-md overflow-hidden transition-opacity duration-500 ${
         isIntersecting ? 'opacity-100' : 'opacity-0'
       }`}
